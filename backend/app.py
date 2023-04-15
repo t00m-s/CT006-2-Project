c331e4fe7f149a31e2e45982157ec83d170d2b8d
michael <michael@busnet.it>
2023-04-15 17:54:00 +0200

    configurazioni

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World prova!'
+    user=os.getenv('POSTGRES_USER')
+    return 'Hello World prova!'+ user
 
 
 if __name__ == '__main__':
