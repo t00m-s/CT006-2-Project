565f8ad04d51990d8cb7087a696a45790804f682
michael <michael@busnet.it>
2023-05-07 00:08:41 +0200

    spostate le notifiche in un unico file

--- code/backend/src/login_register.py
+++ code/backend/src/login_register.py
@@ -5,6 +5,7 @@ from ...frontend.src.utility import render_with_lib
 from ..database.session import *
 from ..database.maps.user import *
 from ..database.maps.role import *
+
 login_register_blueprint = Blueprint('login_register', __name__)
 
 
@@ -14,14 +15,15 @@ def login():
         user = get_session().query(User).filter_by(email=request.form['email']).first()
         if user is None or user.password is None:
             flash("User not found.")
-            return redirect(url_for('index.index')) 
-        if sha512_crypt.verify(request.form['password'], user.password): #TODO: test this
+            return redirect(url_for('index.index'))
+        if sha512_crypt.verify(request.form['password'], user.password):  # TODO: test this
             return redirect(url_for('home.home'))
         else:
             flash("Wrong password.")
             return redirect(url_for('index.index'))
     else:
         return redirect(url_for('index.index'))
+
 
 # TODO fare una cosa fatta bene...
 @login_register_blueprint.route('/register', methods=['GET'])
@@ -31,7 +33,7 @@ def register_page():
 
 @login_register_blueprint.route('/register', methods=['POST'])
 def register_back():
-    if request.method == 'POST': #TODO forse rimuovere questi controlli sotto?
+    if request.method == 'POST':  # TODO forse rimuovere questi controlli sotto?
         if request.form['email'] is None:
             flash("You forgot the email")
             return redirect(url_for('login_register.register_page'))
@@ -55,7 +57,7 @@ def register_back():
         if user is not None and user.password is not None:
             flash("User already registered.")
             return redirect(url_for('login_register.login'))
-        
+
         # dovrebbe essere tutto okay, non abbiamo trovato nessun utente con questa mail
         # creiamo quello nuovo
         new_user = User(name=request.form['name'],
@@ -68,4 +70,4 @@ def register_back():
         get_session().commit()
         return redirect(url_for('login_register.login'))
     else:
-        return redirect(url_for('login_register.register_page')) # TODO: importare bootstrap
+        return redirect(url_for('login_register.register_page'))  # TODO: importare bootstrap
