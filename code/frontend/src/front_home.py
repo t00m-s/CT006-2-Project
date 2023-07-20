16069f78858fdadc2debaa3724082c9d0ac4bcec
Tommaso <soncintommaso@gmail.com>
2023-07-20 17:34:48 +0200

    add: mock project page

--- code/frontend/src/front_home.py
+++ code/frontend/src/front_home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint 
 from utility import render_with_lib
-
+# from ...backend.database.session import get_session  TODO: fix
 home_blueprint = Blueprint('home', __name__, template_folder="../templates")
 
 
@@ -14,4 +14,9 @@ def render_project(user):
     '''
     Renders the project page
     '''
-    return render_with_lib('project.html', user=user)
+    # temp_project = Project(1, 1, "Prova primo progetto")
+    # get_session().add(temp_project)
+    # get_session().commit()
+    # approved_projects = get_session().query(Project).filter_by(id_type=3).first().name
+    return render_with_lib('project.html', user=user, name="Una prova")
+
