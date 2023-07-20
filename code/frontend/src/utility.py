32ae3394e451871eeee59b47921a27279ef68947
Tommaso <soncintommaso@gmail.com>
2023-07-20 16:35:39 +0200

    fix: empty dict on every page

--- code/frontend/src/utility.py
+++ code/frontend/src/utility.py
@@ -26,7 +26,7 @@ def render_with_lib(page, **kwargs):
         '/frontend/static/css/style.css'
     ]
 
-    kwargs['icons'] = [
+    kwargs['icons_css'] = [
             '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />',
             '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />'
     ]
