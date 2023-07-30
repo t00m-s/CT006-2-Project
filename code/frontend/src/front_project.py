00a0a76b5baaa458a702f07bd1bd7593976fb586
Tommaso <soncintommaso@gmail.com>
2023-07-30 12:56:11 +0200

    modified: removed junk from files

--- code/frontend/src/front_project.py
+++ code/frontend/src/front_project.py
@@ -5,7 +5,6 @@ project_blueprint = Blueprint(
     'project', __name__, template_folder="../templates")
 
 
-
 def render_project(user, values):
     '''
     Renders the project page
@@ -14,11 +13,7 @@ def render_project(user, values):
     return render_with_lib('projects.html', user=user, values=values)
 
 
-<<<<<<< HEAD
 def render_viewproject(user, project, files):
-=======
-def render_viewproject(user, project):
->>>>>>> f55a346d982bea39e6c18673ecd72dfb7de6a6d8
     '''
     Renders the viewproject page
     '''
@@ -29,16 +24,14 @@ def render_viewproject(user, project):
     return render_with_lib('viewproject.html', user=user, project=project, files=files, custom_javascript='/frontend/static/js/project.js')
 
 
-
 def render_addproject(user):
     '''
     Renders the addproject pages
     '''
-<<<<<<< HEAD
     return render_with_lib('addproject.html', user=user)
-=======
-    custom_javascript = ['https://unpkg.com/dropzone@5/dist/min/dropzone.min.js', '/frontend/static/js/upload.js']
-    custom_css = ['https://unpkg.com/dropzone@5/dist/min/dropzone.min.css', '/frontend/static/css/upload.css']
+    custom_javascript = [
+        'https://unpkg.com/dropzone@5/dist/min/dropzone.min.js', '/frontend/static/js/upload.js']
+    custom_css = ['https://unpkg.com/dropzone@5/dist/min/dropzone.min.css',
+                  '/frontend/static/css/upload.css']
     return render_with_lib('addproject.html', user=user, custom_javascript=custom_javascript,
                            custom_css=custom_css)
->>>>>>> f55a346d982bea39e6c18673ecd72dfb7de6a6d8
