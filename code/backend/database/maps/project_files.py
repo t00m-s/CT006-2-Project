5e2ea38e5951324a288a1a717531df1b5951430b
Tommaso <soncintommaso@gmail.com>
2023-08-01 13:17:36 +0200

    add: unique path file for files

--- code/backend/database/maps/project_files.py
+++ code/backend/database/maps/project_files.py
@@ -9,10 +9,12 @@ Base = declarative_base()
 class ProjectFiles(Base):
     __tablename__ = "project_files"
     id = Column(Integer, primary_key=True)
-    path = Column(String, nullable=False)
-    id_project_history = Column(Integer, ForeignKey(ProjectHistory.id), nullable=False)
+    path = Column(String, nullable=False, unique=True)
+    id_project_history = Column(Integer, ForeignKey(
+        ProjectHistory.id), nullable=False)
 
     project_history = Relationship(ProjectHistory, back_populates='files')
 
 
-ProjectHistory.files = Relationship(ProjectFiles, back_populates='project_history')
+ProjectHistory.files = Relationship(
+    ProjectFiles, back_populates='project_history')
