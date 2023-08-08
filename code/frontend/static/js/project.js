fc2051987b25bd3d23a986c88cecfc9abdb00c23
Tommaso <soncintommaso@gmail.com>
2023-08-08 13:48:52 +0200

    modified: removed TODO for multiple file types since we only accept PDFs

--- code/frontend/static/js/project.js
+++ code/frontend/static/js/project.js
@@ -1,6 +1,6 @@
 function getFile(fileID) {
   let tmp = "/viewfile/" + fileID;
   document.getElementById("file").innerHTML =
-    '<embed type="application/pdf" src=' + tmp + "></embed>"; // TODO make it work with multiple mime types
+    '<embed type="application/pdf" src=' + tmp + "></embed>";
   document.getElementById("file").classList.remove("d-none");
 }
