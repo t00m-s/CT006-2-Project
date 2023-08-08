284d38310c5cc3485a7173b849eb3c1d2135dc28
Tommaso <soncintommaso@gmail.com>
2023-08-08 11:26:12 +0200

    modified: refactored file viewer button

--- code/frontend/static/js/project.js
+++ code/frontend/static/js/project.js
@@ -2,6 +2,5 @@ function getFile(fileID) {
   let tmp = "/viewfile/" + fileID;
   document.getElementById("file").innerHTML =
     '<embed type="application/pdf" src=' + tmp + "></embed>"; // TODO make it work with multiple mime types
-  document.getElementById("file").style.display = "block";
-  console.log("Button clicked: " + fileID);
+  document.getElementById("file").classList.remove("d-none");
 }
