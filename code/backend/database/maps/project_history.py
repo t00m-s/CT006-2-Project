337049b65ad62ed1a4729a555a89b3ff16ef331e
Tommaso <soncintommaso@gmail.com>
2023-09-01 22:25:46 +0200

    add: explicit nullable to project_history ORM

--- code/backend/database/maps/project_history.py
+++ code/backend/database/maps/project_history.py
@@ -11,18 +11,17 @@ Base = declarative_base()
 
 
 class ProjectHistory(Base):
-    __tablename__ = 'project_history'
+    __tablename__ = "project_history"
     id = Column(Integer, primary_key=True)
     id_project = Column(Integer, ForeignKey(Project.id), nullable=False)
     id_state = Column(Integer, ForeignKey(State.id), nullable=False)
     id_user_reviewer = Column(Integer, ForeignKey(User.id), nullable=True)
-    note = Column(Text)
-    created_at = Column(DateTime, default=func.now(),
-                        server_default=func.now())
+    note = Column(Text, nullable=True)
+    created_at = Column(DateTime, default=func.now(), server_default=func.now())
 
-    reviewer = Relationship(User, back_populates='reviewed_histories')
-    state = Relationship(State, back_populates='attributed_histories')
-    project = Relationship(Project, back_populates='histories')
+    reviewer = Relationship(User, back_populates="reviewed_histories")
+    state = Relationship(State, back_populates="attributed_histories")
+    project = Relationship(Project, back_populates="histories")
 
     def __repr__(self):
         return f"<ProjectHistory(id={self.id}, id_project={self.id_project}, id_state={self.id_state}, created_at={self.created_at})>"
@@ -31,8 +30,8 @@ class ProjectHistory(Base):
         return self.state.is_closed
 
 
-Project.histories = relationship(ProjectHistory, back_populates='project', order_by=ProjectHistory.id.desc())
-User.reviewed_histories = relationship(
-    ProjectHistory, back_populates='reviewer')
-State.attributed_histories = relationship(
-    ProjectHistory, back_populates='state')
+Project.histories = relationship(
+    ProjectHistory, back_populates="project", order_by=ProjectHistory.id.desc()
+)
+User.reviewed_histories = relationship(ProjectHistory, back_populates="reviewer")
+State.attributed_histories = relationship(ProjectHistory, back_populates="state")
