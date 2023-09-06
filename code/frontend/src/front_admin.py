79b2a02175ea080cb83ddfc202f54d92a79a7f02
Tommaso <soncintommaso@gmail.com>
2023-09-06 15:48:33 +0200

    add: editrole and banuser routes, login checks for banned users

--- code/frontend/src/front_admin.py
+++ code/frontend/src/front_admin.py
@@ -4,14 +4,14 @@ from utility import render_with_lib
 admin_blueprint = Blueprint("admin", __name__, template_folder="../templtates")
 
 
-def render_admin(user, users_list, columns):
+def render_admin(user, users_list, columns, roles):
     """
     Renders the admin page
 
     @params user Current user logged in
     @params users_list Other users that can be edited
     """
-    custom_javscript = [
+    custom_javascript = [
         "/frontend/static/js/admin.js",
         "https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js",
         "https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js",
@@ -21,6 +21,7 @@ def render_admin(user, users_list, columns):
         "admin.html",
         users=users_list,
         columns=columns,
-        custom_javascript=custom_javscript,
+        roles=roles,
+        custom_javascript=custom_javascript,
         custom_css=custom_css,
     )
