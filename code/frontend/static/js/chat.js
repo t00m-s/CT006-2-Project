ba005a754e8947b85267693c588501ab45f8f91b
michael <michael@busnet.it>
2023-09-23 16:24:40 +0200

    FIx la chat ha bisogno di un messaggio per essere inizializzata

--- code/frontend/static/js/chat.js
+++ code/frontend/static/js/chat.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     const socket = io.connect('http://localhost:5001', {query: 'id_project=' + id_project});
 
     socket.on('connect', function () {
-        // socket.send({message: 'user has connected!', id_project: id_project});
+        socket.send({message: 'user has connected!', id_project: id_project});
     });
 
     socket.on('message', function (msg) {
